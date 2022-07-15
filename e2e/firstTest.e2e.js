4fbf3fb4d624efa19a1e7a3e3c7f96f8c5ff618b
aleucilima <aleuci.lima@gmail.com>
2022-07-15 19:38:58 -0300

    update: detox build tests

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,4 +1,4 @@
-describe('My firts tests', () => {
+describe('Example', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -11,4 +11,13 @@ describe('My firts tests', () => {
     await expect(element(by.id('welcome'))).toBeVisible();
   });
 
+  it('should show hello screen after tap', async () => {
+    await element(by.id('hello_button')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
+
+  it('should show world screen after tap', async () => {
+    await element(by.id('world_button')).tap();
+    await expect(element(by.text('World!!!'))).toBeVisible();
+  });
 });
