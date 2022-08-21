404c547f17de921d65f9c77bd1b0c18538e218ce
aleucilima <aleuci.alves@theos.com.br>
2022-08-20 22:01:20 -0300

    feat: splashscreen

--- android/app/src/main/java/com/myskills/MainActivity.java
+++ android/app/src/main/java/com/myskills/MainActivity.java
@@ -2,20 +2,19 @@ package com.myskills;
 
 import com.facebook.react.ReactActivity;
 
-import android.os.Bundle; // adicionado
-import org.devio.rn.splashscreen.SplashScreen; // adicionado
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen; 
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  
+      super.onCreate(savedInstanceState);
+  }
 
   @Override
   protected String getMainComponentName() {
     return "myskills";
   }
-
-		// adicionado
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  
-        super.onCreate(savedInstanceState);
-    }
 }
